9d27901db51ae231e2c57c4da32a5d47c2f24874
Sammi-Jo Lee <sjlee3157@gmail.com>
2018-08-14 15:16:14 -0700

    renamed variable players_hand_of_ten to letter_bank

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -2,15 +2,19 @@ def draw_letters
   letters_to_quantity = {"a" => 9, "b" => 2, "c" => 2, "d" => 4, "e" => 12, "f" => 2, "g" => 3, "h"=> 2, "i" => 9, "j" => 1, "k" => 1,
     "l" => 4, "m" => 2, "n" => 6, "o" => 8, "p" => 2, "q"=> 1, "r" => 6, "s" => 4, "t" => 6, "u"=> 4, "v" => 2,
     "w" => 2, "x" => 1, "y"=> 2, "z" => 1}
-  players_hand_of_ten = Array.new
+  letter_bank= Array.new
   10.times do
     random_letter_picked = letters_to_quantity.keys.sample
-    players_hand_of_ten << random_letter_picked
-    while players_hand_of_ten.count(random_letter_picked) > letters_to_quantity[random_letter_picked]
-      players_hand_of_ten.pop
+    letter_bank << random_letter_picked
+    while letter_bank.count(random_letter_picked) > letters_to_quantity[random_letter_picked]
+      letter_bank.pop
       random_letter_picked = letters_to_quantity.keys.sample
-      players_hand_of_ten << random_letter_picked
+      letter_bank << random_letter_picked
     end
   end
-  return players_hand_of_ten
+  return letter_bank
+end
+
+def uses_available_letters?(user_input_word, letter_bank)
+
 end
